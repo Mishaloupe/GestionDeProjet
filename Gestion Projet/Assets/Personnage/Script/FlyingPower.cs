fb7e9f35482ffb9cd34bdff3c193f8efd8b5bc4a
Mishaloupe <147034100+Mishaloupe@users.noreply.github.com>
2025-03-12 15:47:42 +0100

    Revert "fix older"
    This reverts commit 2b8a92d5d87bedd081af6a84517544505d43fc66.

--- Gestion Projet/Assets/Personnage/Script/FlyingPower.cs
+++ Gestion Projet/Assets/Personnage/Script/FlyingPower.cs
@@ -56,7 +56,7 @@ public class FlyOnCollision : MonoBehaviour
             if (Input.GetKey(KeyCode.LeftShift)) moveY = -1f;
 
             Vector3 moveDirection = new Vector3(moveX, moveY, moveZ).normalized;
-            rb.velocity = moveDirection * flySpeed;
+            rb.linearVelocity = moveDirection * flySpeed;
         }
     }
 }
